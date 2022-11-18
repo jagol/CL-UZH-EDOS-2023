2556f61d62425bf2432cd10547a76142c99aee28
Janis Goldzycher <janisgoldzycher@mailbox.org>
2022-11-18 16:49:28 +0100

    add dataset_io module

---
+++ src/dataset_io.py
@@ -0,0 +1,70 @@
+import csv
+import json
+from typing import List, Dict, Any, Optional
+
+
+class Loader:
+    
+    @staticmethod
+    def load(fpath: str) -> List[Dict[str, Any]]:
+        raise NotImplementedError
+
+
+class Writer:
+    
+    @staticmethod
+    def write(entries: List[Any], fpath: str, header: Optional[List[str]]) -> None:
+        raise NotImplementedError
+
+
+class JSONLLoader(Loader):
+    
+    @staticmethod
+    def load(fpath: str) -> List[Dict[str, Any]]:
+        entries = []
+        with open(fpath) as fin:
+            for line in fin:
+                entries.append(json.loads(line))
+        return entries
+
+
+class JSONLWriter(Writer):
+    
+    @staticmethod
+    def write(entries: List[Any], fpath: str) -> None:
+        entries = []
+        with open(fpath, 'w') as fout:
+            for entry in entries:
+                fout.write(json.dumps(entry) + '\n')
+
+
+class EDOSCSVLoader(Loader):
+    
+    @staticmethod
+    def load(fpath: str) -> List[Dict[str, Any]]:
+        entries = []
+        with open(fpath) as fin:
+            reader = csv.DictReader(fin)
+            for row in reader:
+                entries.append(row)
+        return entries
+
+
+class EDOSCSVWriter(Writer):
+
+    @staticmethod
+    def write(entries: List[Dict[str, Any]], fpath: str, header: Optional[List[str]]) -> None:
+        with open(fpath, 'w') as fout:
+            writer = csv.DictWriter(fout)
+            writer.writeheader(header)
+            for entry in entries:
+                writer.writerow(entry)
+
+
+LOADERS = {
+    'EDOSCSVLoader': EDOSCSVLoader
+}
+
+WRITERS = {
+    'EDOSCSVWriter': EDOSCSVWriter
+}
