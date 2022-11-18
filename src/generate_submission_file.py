de77c278bae58b346424b001e26323ddb36eb2fa
Janis Goldzycher <janisgoldzycher@mailbox.org>
2022-11-18 14:05:03 +0100

    add option "convert to int" to script

--- src/generate_submission_file.py
+++ src/generate_submission_file.py
@@ -4,6 +4,7 @@ import json
 import os
 from typing import Dict, Any
 
+from evaluate_predictions import to_int
 import mappings
 
 
@@ -24,14 +25,16 @@ def main(args: argparse.Namespace) -> None:
         mapping = mappings.VEC_LABEL_NUM_TO_STR
     else:
         raise Exception(f'Unknown Task: {args.task}')
-    # input_fn = args.input.split('/')[-1]
-    # input_fn_without_ending = input_fn[:-6]
     preds = load_preds(args.input)
     with open(args.output, 'w') as fout:
         writer = csv.writer(fout)
         writer.writerow(['rewire_id', 'label_pred'])
         for pred in preds:
-            label_str = mapping[pred[args.prediction_key]]
+            if args.to_int:
+                pred_int = to_int(pred[args.prediction_key], args.threshold)
+            else:
+                pred_int = pred[args.prediction_key]
+            label_str = mapping[pred_int]
             writer.writerow([pred['id'], label_str])
 
 
@@ -42,5 +45,9 @@ if __name__ == '__main__':
     parser.add_argument('-o', '--output', help='Path to output file in csv-format.')
     parser.add_argument('-p', '--prediction_key', 
                         help='Key that holds the prediction for each json-line/item.')
+    parser.add_argument('--to_int', action='store_true', 
+                        help='If predictions are floats, use this flag to convert the floats (between 0 and 1] to an int.')
+    parser.add_argument('--threshold', type=float, default=0.5, 
+                        help='Threshold used when converting a float to an integer.')
     cmd_args = parser.parse_args()
     main(cmd_args)
