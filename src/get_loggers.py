fa80cad5b2677caf7e8be2310a3096e915a5c2b0
Janis Goldzycher <janisgoldzycher@mailbox.org>
2022-12-08 13:41:55 +0100

    adjust logging directory

--- src/get_loggers.py
+++ src/get_loggers.py
@@ -7,7 +7,7 @@ import sys
 def get_logger(level='main') -> logging.Logger:
     log_formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s')
     root_logger = logging.getLogger(level)
-    file_handler = logging.FileHandler(os.path.join('.', f'{level}_logs.txt'))
+    file_handler = logging.FileHandler(os.path.join('logs', f'{level}_logs.txt'))
     file_handler.setFormatter(log_formatter)
     root_logger.addHandler(file_handler)
     console_handler = logging.StreamHandler(sys.stdout)
